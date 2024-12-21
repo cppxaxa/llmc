1e5b8a9133fc908ba9487a8261b331591532f383
prateekxaxa <prateekxaxa@microsoft.com>
2024-12-22 01:12:01 +0530

    Add AppendUndo class with Execute method in llmc.Executor
    Added a new AppendUndo class in the llmc.Executor namespace,
    inheriting from ExecutorCommon. Implemented the Execute method
    to construct and return an "undo" operation string. The method
    parses the param string into a dictionary, processes the source
    filename and other parameters. Included necessary using
    directives for llmc.Project and System.Text.

---
+++ llmc/Executor/AppendUndo.cs
@@ -0,0 +1,21 @@
+﻿using llmc.Project;
+using System.Text;
+
+namespace llmc.Executor;
+
+internal class AppendUndo : ExecutorCommon
+{
+    public override string Execute(string parentDirectory, string param)
+    {
+        StringBuilder undo = new();
+
+        Dictionary<string, string> p = Common.ParseParam(param);
+        string source = p["fn"];
+        List<string> remainingParams = p.Where(e => e.Key != "fn")
+            .Select(e => $"{e.Key}=\"{e.Value}\"").ToList();
+
+        undo.Append($"{source}({string.Join(',', remainingParams)})");
+        
+        return undo.ToString();
+    }
+}
